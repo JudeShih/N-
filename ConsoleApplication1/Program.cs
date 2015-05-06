a217f3efed3edaa35dec0ca8b88321d14304e89b
JudeShih <jackshih5828@gmail.com>
2015-05-06 14:21:36 +0800

    N!

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 1; int i = 0;
+            int Number = int.Parse(Console.ReadLine());
+            while (i+1 <= Number)
+            {
+                sum = ++i * sum;
+            }
+            Console.WriteLine("X! = "+sum);
+        }
+    }
+}
